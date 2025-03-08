1c484c336cd22584f7cd3525aca4c44dbfae00c5
Valor Kand <david.tab2023@icloud.com>
2025-03-07 22:30:42 -0600

    Añadido primer pantallazo
    Primera pantalla

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,7 +6,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    app.http.server.configuration.hostname = "172.16.16.217"
+    app.http.server.configuration.hostname = "0.0.0.0"
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
